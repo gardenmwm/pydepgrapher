c89ace12642e6b28ce88898484f962d8f0f60d89
gardenmwm@gmail.com <gardenmwm@gmail.com@ca4c9b45-da16-1c6c-8dbf-81f1af7eba53>
2012-06-01 23:43:55 +0000

    more screwups

--- pydepgrapher.py
+++ pydepgrapher.py
@@ -14,79 +14,11 @@ Graphviz		http://www.graphviz.org
 @author: gardenmwm
 '''
 import pydot
-import csv
-import ConfigParser
+import dg
+
 import sys
 
-def GetNodeOptions(parser):
-    Opts={}
-    NodeOpts={}
-    #Get Node Types
-    NodeTypes=parser.get('types', 'NodeTypes').split(',')
-    for nt in NodeTypes:
-        #Only pull options for ones that have sections
-        if parser.has_section(nt):
-            for name,value in parser.items(nt):
-                NodeOpts[name]=value
-            Opts[nt]=NodeOpts
-            NodeOpts={}
-    #print Opts
-    return Opts
 
-def GetEdgeOptions(parser):
-    Opts={}
-    EdgeOpts={}
-    #Get Node Types
-    EdgeTypes=parser.get('types', 'DepTypes').split(',')
-    for et in EdgeTypes:
-        #Only pull options for ones that have sections
-        if parser.has_section(et):
-            for name,value in parser.items(et):
-                EdgeOpts[name]=value
-            Opts[et]=EdgeOpts
-            EdgeOpts={}
-    #print Opts
-    return Opts
-
-def GetGraphOptions(parser):
-    Opts={}
-    Opts['graph_type']='digraph'
-    for name,value in parser.items('graph'):
-        Opts[name]=value
-    return Opts
-
-def readsource(sourcefile):
-    deplist=[]
-    f=open(sourcefile,'rt')
-    reader = csv.DictReader(f)
-    for row in reader:
-        deplist.append(row)
-    return deplist
-
-def getnodes(deps):
-    nodelist={}
-    for dep in deps:
-        nodelist[dep['resource']]=dep['resource_type']
-        if  dep['dependency'] not in nodelist and dep['dependency'] != '':
-	       nodelist[dep['dependency']]=''
-    return nodelist
-
-def getclusters(deps):
-    clusters={}
-    for dep in deps:
-        if dep['cluster'] != '' and dep['cluster'] in clusters:
-            clusters[dep['cluster']].append(dep['resource'])
-        elif dep['cluster'] != '' and dep['cluster'] not in clusters:
-            clusters[dep['cluster']]=[dep['resource']]
-    return clusters
-
-def getedges(deps):
-    edgelist=[]
-    for dep in deps:
-        if dep['dependency'] != '':
-            edge=[dep['resource'],dep['dependency'],dep['dependency_type']]
-            edgelist.append(edge)
-    return edgelist
 
 def makeedge(node1,node2,edgetype):
     pe=pydot.Edge(node1,node2)
@@ -102,47 +34,14 @@ def makenode(nodestring,nodetype):
             pn.set(k, v)
     return pn
 
-def getdepnodes(node,deps,depth=100):
-    '''This will return a list of node strings that are dependant on this node, or an upwards direction'''
-    nodelist=[node]
-    while depth > 0:
-        depth -= 1
-        for dep in deps:
-            if dep['dependency'] in nodelist and dep['resource'] not in nodelist:
-                nodelist.append(dep['resource'])
-    return nodelist
 
-def getnodedeps(node,deps,depth=100):
-    '''This will return a list of nodes that the specified node is dependent on, or a downwards direction'''
-    nodelist=[node]
-    while depth > 0:
-        depth -= 1
-        for dep in deps:
-            if dep['resource'] in nodelist and dep['dependency'] not in nodelist:
-                nodelist.append(dep['dependency'])
-    return nodelist
+
+
 
 if __name__=="__main__":
-   #Parse Config
-   print "Parsing Config"
-   parser = ConfigParser.SafeConfigParser()
-   parser.read('config.ini')
-   sourcefile=parser.get('general', 'sourcefile')
-   outfile=parser.get('general','outfile')
-   NodeOptions=GetNodeOptions(parser)
-   EdgeOptions=GetEdgeOptions(parser)
-   GraphOptions=GetGraphOptions(parser)
-   #Get Deps from file and into memory
-   print "Reading Dep file"
-   deps=readsource(sourcefile)
-   print "Reading Nodes"
-   nodestrings=getnodes(deps)
-   print "Reading Edges"
-   edges=getedges(deps)
-   clusterstrings=getclusters(deps)
    
    #Build Nodes
-   print "Building Nodes"
+   dg.depconfig()
    nodelist={}
    for node in nodestrings:
        nodelist[node]=makenode(node,nodestrings[node])
