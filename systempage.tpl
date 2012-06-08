cecb9937840dfef20aeece9f2ae4f5ae6bf172d5
gardenmwm@gmail.com <gardenmwm@gmail.com@ca4c9b45-da16-1c6c-8dbf-81f1af7eba53>
2012-06-08 17:43:46 +0000

    More Complete website, now shows graph for deps and reqs

--- systempage.tpl
+++ systempage.tpl
@@ -1,9 +1,15 @@
 <HTML>
 <TITLE>Dependencies for {{system}}</TITLE>
 <Body>
-<a href=maps/{{system}}.png><img src=maps/{{system}}.png width=600 height=500></a>
+<a href=maps/deps{{system}}.png><img src=maps/deps{{system}}.png width=600 height=500></a>
+<a href=maps/reqs{{system}}.png><img src=maps/reqs{{system}}.png width=600 height=500></a>
 <table><tr><td>Depends</td></tr>
 %for node in deps:
+	<tr><td><a href='./{{node}}'>{{node}}</a></tr></td>
+%end
+</table>
+<table><tr><td>Reqs</td></tr>
+%for node in reqs:
 	<tr><td><a href='./{{node}}'>{{node}}</a></tr></td>
 %end
 </table>
--- webui.py
+++ webui.py
@@ -13,20 +13,37 @@ def systempage(system):
     ta = {'system':system}
     ta['deps'] = dg.getdepnodes(system, deplist.deps)
     ta['reqs'] = dg.getnodedeps(system, deplist.deps)
+    #Create Deplist and graph
     depslist = dg.depstrings(dgconf.GeneralOptions)
     newdeps = []
     for depline in depslist.deps:
         if depline['resource'] in ta['deps']:
             newdeps.append(depline)
+    for depline in newdeps:
+        if depline['dependency'] not in ta['deps']:
+            depline['dependency'] = ''
     depslist.deps = newdeps
-    print newdeps
     depslist.clusters = depslist.getclusters(depslist.deps)
     depslist.edges = depslist.getedges(depslist.deps)
     depslist.nodes = depslist.getnodes(depslist.deps)
-    print depslist.nodes
-    dgconf.GeneralOptions['outfile'] = 'maps/' + system + '.png'
+    dgconf.GeneralOptions['outfile'] = 'maps/deps' + system + '.png'
     buildgraph(dgconf, depslist)
-    out = template('systempage', deps = ta['deps'], system = system)
+    #Create ReqList
+    reqslist = dg.depstrings(dgconf.GeneralOptions)
+    newreqs = []
+    for depline in reqslist.deps:
+        if depline['resource'] in ta['reqs']:
+            newreqs.append(depline)
+    #for depline in newreqs:
+    #    if depline['resource'] not in ta['reqs']:
+    #        depline['resource'] = ''
+    depslist.deps = newreqs
+    depslist.clusters = depslist.getclusters(depslist.deps)
+    depslist.edges = depslist.getedges(depslist.deps)
+    depslist.nodes = depslist.getnodes(depslist.deps)
+    dgconf.GeneralOptions['outfile'] = 'maps/reqs' + system + '.png'
+    buildgraph(dgconf, depslist)
+    out = template('systempage', deps = ta['deps'], reqs = ta['reqs'], system = system)
     return out
 
 @route('/maps/<path:path>')
@@ -37,4 +54,4 @@ dgconf = dg.depconfig()
 deplist = dg.depstrings(dgconf.GeneralOptions)
 
 if __name__ == '__main__':
-    run(host = 'localhost', port = 8080, debug = True)
+    run(host = '159.36.2.46', port = 8080, debug = True)
